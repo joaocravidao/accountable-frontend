3451d53a5602dfd2d473979c52cbadce782b9b7a
João Cravidão <joao_kravidao@hotmail.com>
2023-11-28 14:44:41 +0000

    css update

--- src/Pages/DashboardPage/index.jsx
+++ src/Pages/DashboardPage/index.jsx
@@ -7,19 +7,19 @@ function DashboardPage() {
         <div id='dashboard'> 
         <div id='daily-todo'>
           <h2>Daily To-Do</h2>
-          <div id="daily-todo-task">
+          <div className="daily-todo-task">
             <h3>Task #1</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
             Find a badass job.</p>
           </div>
-          <div>
+          <div className='daily-todo-task-2'>
             <h3>Task #2</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
             Find a badass job.</p>
           </div>
-          <div>
+          <div className='daily-todo-task-2'>
             <h3>Task #3</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
@@ -28,19 +28,19 @@ function DashboardPage() {
         </div>
         <div id='friends-todo'>
           <h2>Friends Overview</h2>
-          <div>
+          <div className="daily-todo-task">
             <h3>Friends Task #1</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
             Learn playing guitar</p>
           </div>
-          <div>
+          <div className='daily-todo-task-2'>
             <h3>Friends Task #2</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
             Learn playing guitar</p>
           </div>
-          <div>
+          <div className='daily-todo-task-2'>
             <h3>Friends Task #3</h3>
             <p><b>Deadline: </b> 20.12.2023</p>
             <p><b>Description: </b> 
