462fd38f854c4ddbf077372c398fa96c616b6c4d
João Cravidão <joao_kravidao@hotmail.com>
2023-11-28 11:57:53 +0000

    page creation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage'
+
+function App() {
+  return (
+    <Routes>
+          <Route path="/login" element={<LoginPage />} />
+       </Routes>
+  )
+}
+
+export default App
