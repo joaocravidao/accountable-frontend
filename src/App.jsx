2c732937a4c12d63e8df581ba6e0b9f9fb6d9a34
João Cravidão <joao_kravidao@hotmail.com>
2023-11-28 12:19:22 +0000

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './index.css'
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './Pages/LoginPage'
+import DashboardPage from './Pages/DashboardPage'
 
 function App() {
   return (
     <Routes>
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<DashboardPage />} />
        </Routes>
   )
 }
