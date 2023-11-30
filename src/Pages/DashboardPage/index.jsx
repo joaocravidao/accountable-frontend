47946f78590e5e06f74e16c5a0c5ce9b630b504f
Jakob Bodendieck <bodendieck.jakob@gmail.com>
2023-11-30 16:42:58 +0000

    delete task working

--- src/Pages/DashboardPage/index.jsx
+++ src/Pages/DashboardPage/index.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import ToDoCard from "../../components/ToDoCard"
+import React, { useEffect, useState } from 'react';
+import ToDoCard from '../../components/ToDoCard';
 import { getAllToDo } from '../../utils/HandleApi';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { BiEdit } from 'react-icons/bi';
+import { AiFillDelete } from 'react-icons/ai';
 
-const API_URL = "http://localhost:5005";
+const API_URL = 'http://localhost:5005';
 
 const taskValues = {
-  title: "",
-  description: "",
-  deadline: "2024-01-01",
-  attachments: "", 
-}
+  title: '',
+  description: '',
+  deadline: '2024-01-01',
+  attachments: '',
+};
 
 function DashboardPage() {
-
-	const [toDo, setToDo] = useState({...taskValues})
+  const [toDo, setToDo] = useState({ ...taskValues });
   const [toDoList, setToDoList] = useState([]);
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    const {name, value} = e.target;  //still need to determine this! 
-
-    setToDo((prevToDo)=>({ //this too! 
+    const { name, value } = e.target;
+    setToDo((prevToDo) => ({
       ...prevToDo,
       [name]: value,
-    }))
-  }
+    }));
+  };
 
   const fetchData = () => {
     axios
@@ -44,10 +44,7 @@ function DashboardPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const requestBody = {
-      ...toDo,
-    };
+    const requestBody = { ...toDo };
 
     axios
       .post(`${API_URL}/api/task`, requestBody)
@@ -59,36 +56,63 @@ function DashboardPage() {
       .catch((error) => console.log(error));
   };
 
-
-
+  const handleDelete = (taskId) => {
+    axios
+      .delete(`${API_URL}/api/tasks/${taskId}`)
+      .then(() => fetchData()) // Call the function here to fetch updated data
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className='container'>
       <h1>Dashboard Page</h1>
       <div className='top'>
-        <form
-        onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <h3>Create Task</h3>
-        <input type="text" name="title" value={toDo.title} onChange={handleChange} placeholder='title'></input>
-        <input type="text" name="description" value={toDo.description} onChange={handleChange} placeholder='description'></input>
-        <input type="date" name="deadline" value={toDo.deadline} onChange={handleChange} placeholder='deadline'></input>
-        <input type="text" name="attachments" value={toDo.attachments} onChange={handleChange} placeholder='attachments'></input>
-        <button
-          type="submit"
-        >
-          Create ToDo
-        </button>
+          <input
+            type='text'
+            name='title'
+            value={toDo.title}
+            onChange={handleChange}
+            placeholder='title'
+          />
+          <input
+            type='text'
+            name='description'
+            value={toDo.description}
+            onChange={handleChange}
+            placeholder='description'
+          />
+          <input
+            type='date'
+            name='deadline'
+            value={toDo.deadline}
+            onChange={handleChange}
+            placeholder='deadline'
+          />
+          <input
+            type='text'
+            name='attachments'
+            value={toDo.attachments}
+            onChange={handleChange}
+            placeholder='attachments'
+          />
+          <button type='submit'>Create ToDo</button>
         </form>
         {toDoList &&
-        toDoList.map(
-          (toDo, index) => (
-              <ToDoCard
-                key={toDo._id}
-                {...toDo}
-                className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
+          toDoList.map((toDo, index) => (
+            <div
+              className={index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}
+              key={toDo._id}
+            >
+              <div className='text'>{toDo.text}</div>
+              <BiEdit className='icon' />
+              <AiFillDelete
+                className='icon'
+                onClick={() => handleDelete(toDo._id)}
               />
-          )
-        )}
+            </div>
+          ))}
       </div>
     </div>
   );
--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import {BiEdit} from 'react-icons/bi'
 import {AiFillDelete} from 'react-icons/ai'
 
-const ToDo = ({text, updateMode, deleteToDo}) => {
+
+
+const ToDo = ({text, deleteToDo}) => {
+
   return (
     <div className='todo'>
         <div className='text'>{text}</div>
-        <div className='icons'>
-            <BiEdit className='icon' onClick={updateMode} />
-            <AiFillDelete className='icon' onClick={deleteToDo} />
-        </div>
     </div>
   )
 }
